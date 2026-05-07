cc80f1654488c1cb6ad9bb800fe55b76c4a0c57b
Josepuadev <josepuadev@gmail.com>
2026-05-07 15:28:16 +0000

    V_0_1_5 : creados los modelos en la API

--- spartan-api/app/Models/Ejercicio.php
+++ spartan-api/app/Models/Ejercicio.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Ejercicio extends Model
 {
-    //
+    public function musculos() {
+        return $this->belongsToMany(Musculo::class, 'ejercicios_musculos');
+        // Relacionamos la tabla musculos con ejercicios creando una tabla intermedia llamada ejercicios_musculos. 
+    }
 }
--- spartan-api/app/Models/Musculo.php
+++ spartan-api/app/Models/Musculo.php
@@ -3,8 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Musculo extends Model
 {
-    //
+    public function ejercicios() {
+        return $this->belongsToMany(Ejercicio::class, 'ejercicios_musculos');
+    }
 }
