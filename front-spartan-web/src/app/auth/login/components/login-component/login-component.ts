32bdf624cd1b2dd4e04587d57cd8dd8c240d4db5
Josepuadev <josepuadev@gmail.com>
2026-05-12 10:12:25 +0000

    V_0_2_1 : añadida validaciones en el formulario de login y registro

--- front-spartan-web/src/app/auth/login/components/login-component/login-component.ts
+++ front-spartan-web/src/app/auth/login/components/login-component/login-component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Authservice } from '../../../../services/auth/authservice';
 
@@ -17,11 +17,37 @@ export class LoginComponent {
   mensajeError : string | null = null;
 
   formulario: FormGroup = this.formBuilder.group({
-    email:                  [null],
-    password:               [null],
+    email:                  [null, [
+                              Validators.required, 
+                              Validators.email, 
+                              Validators.maxLength(255)
+                            ]],
+    password:               [null, [
+                              Validators.required, 
+                              Validators.minLength(8)
+                            ]],
   })
 
+  validadorMensajesError(campoFormulario: string): string {
+    const controlErrores = this.formulario.get(campoFormulario);
+
+    if (!controlErrores?.invalid || !controlErrores?.touched) return '';
+
+    if (controlErrores.errors?.['required'])    return 'Campo olbligatorio';
+    if (controlErrores.errors?.['email'])       return 'Email no valido';
+    if (controlErrores.errors?.['minlength'])   return 'Mínimo 8 caracteres';
+    if (controlErrores.errors?.['pattern'])     return 'Solo letras, numeros y espacios';
+
+    return 'Campo invalido';
+
+  }
+
   onSubmit(): void {
+
+    if(this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
     
     this.authService.logearse(this.formulario.value).subscribe({
 
--- front-spartan-web/src/app/auth/register/components/register-component/register-component.ts
+++ front-spartan-web/src/app/auth/register/components/register-component/register-component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Authservice } from '../../../../services/auth/authservice';
 
@@ -19,14 +19,58 @@ export class RegisterComponent {
   mensajeError : string | null = null;
 
   formulario: FormGroup = this.formBuilder.group({
-    name:                   [null],
-    email:                  [null],
-    password:               [null],
-    password_confirmation:  [null],
-  })
+    name:                   [null, [
+                              Validators.required, 
+                              Validators.maxLength(100), 
+                              Validators.pattern(/^[0-9a-zA-ZÀ-ÿ\s]+$/) 
+                            ]],
+    email:                  [null, [
+                              Validators.required, 
+                              Validators.email, 
+                              Validators.maxLength(255)
+                            ]],
+    password:               [null, [
+                              Validators.required, 
+                              Validators.minLength(8)
+                            ]],
+    password_confirmation:  [null, [
+                              Validators.required, 
+                              Validators.minLength(8)
+                            ]],
+  }, {
+    validators: passwordsCoinciden
+  });
+
+  validadorMensajesError(campoFormulario: string): string {
+    const controlErrores = this.formulario.get(campoFormulario);
+
+    if (!controlErrores?.invalid || !controlErrores?.touched) {
+      
+      if (campoFormulario === 'password_confirmation'
+          && this.formulario.errors?.['noCoinciden']
+          && controlErrores?.touched) {
+            return 'Las contraseñas no coinciden';
+          }
+    
+        return '';
+    }
+
+    if (controlErrores.errors?.['required'])    return 'Campo olbligatorio';
+    if (controlErrores.errors?.['email'])       return 'Email no valido';
+    if (controlErrores.errors?.['minlength'])   return 'Mínimo 8 caracteres';
+    if (controlErrores.errors?.['pattern'])     return 'Solo letras, numeros y espacios';
+
+    return 'Campo invalido';
+
+  }
 
   onSubmit(): void {
     
+    if (this.formulario.invalid) {
+      this.formulario.markAllAsTouched();
+      return;
+    }
+
     this.authService.registrarse(this.formulario.value).subscribe({
 
       next: (respuesta) => {
@@ -45,3 +89,12 @@ export class RegisterComponent {
     
   }
 }
+
+function passwordsCoinciden(control: AbstractControl): ValidationErrors | null {
+  
+  const password = control.get('password')?.value;
+  const password_confirmation = control.get('password_confirmation')?.value;
+
+  return password === password_confirmation ? null : { noCoinciden: true};
+}
+
